9efe5d6048cdd2573d1aa2f8450d189d619b4921
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-22 15:55:54 +0530

    fixed teleportation bug

--- Assets/Scripts/Enemy/Robot/States/RobotTeleportingState.cs
+++ Assets/Scripts/Enemy/Robot/States/RobotTeleportingState.cs
@@ -32,12 +32,12 @@ namespace StatePattern.Enemy.States
 
         private Vector3 GetRandomNavMeshPoint()
         {
-            Vector3 randomDirection = Random.insideUnitSphere * 10f + Owner.Position;
-
+            Vector3 randomDirection = Random.insideUnitSphere * 5f + Owner.Position;
             NavMeshHit hit;
-            NavMesh.SamplePosition(randomDirection, out hit, 10f, NavMesh.AllAreas);
-
-            return hit.position;
+            if (NavMesh.SamplePosition(randomDirection, out hit, 5f, NavMesh.AllAreas))
+                return hit.position;
+            else
+                return Owner.Data.SpawnPosition;
         }
     }
 }
