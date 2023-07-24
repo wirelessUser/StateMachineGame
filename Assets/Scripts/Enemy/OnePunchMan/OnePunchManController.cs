c6d0f832451b43c3e2624230006eff2287cae45e
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-24 17:22:09 +0530

    Added Idle and Rotating state to OnPunchMan using booleans

--- Assets/Scripts/Enemy/OnePunchMan/OnePunchManController.cs
+++ Assets/Scripts/Enemy/OnePunchMan/OnePunchManController.cs
@@ -6,22 +6,74 @@ namespace StatePattern.Enemy
 {
     public class OnePunchManController : EnemyController
     {
+        private bool isIdle;
+        private bool isRotating;
+        private bool isShooting;
+        private float idleTimer;
+        private float targetRotation;
+
         public OnePunchManController(EnemyScriptableObject enemyScriptableObject) : base(enemyScriptableObject)
         {
             enemyView.SetController(this);
+            InitializeVariables();
+        }
+
+        private void InitializeVariables()
+        {
+            isIdle = true;
+            isRotating = false;
+            isShooting = false;
+            idleTimer = enemyScriptableObject.IdleTime;
         }
 
         public override void UpdateEnemy()
         {
             if (currentState == EnemyState.DEACTIVE)
                 return;
+
+            if(isIdle)
+            {
+                idleTimer -= Time.deltaTime;
+                if(idleTimer <= 0)
+                {
+                    isIdle = false;
+                    isRotating = true;
+                    targetRotation = (Rotation.eulerAngles.y + 180) % 360;
+                }
+            }
+
+            if(isRotating)
+            {
+                SetRotaion(CalculateRotate());
+                if(IsRotationComplete())
+                {
+                    isIdle = true;
+                    isRotating = false;
+                    ResetTimer();
+                }
+            }
+
         }
+
+        private void ResetTimer() => idleTimer = enemyScriptableObject.IdleTime;
+
+        private Vector3 CalculateRotate() => Vector3.up * Mathf.MoveTowardsAngle(Rotation.eulerAngles.y, targetRotation, enemyScriptableObject.RotationSpeed * Time.deltaTime);
+
+        private bool IsRotationComplete() => Mathf.Abs(Mathf.Abs(Rotation.eulerAngles.y) - Mathf.Abs(targetRotation)) < Data.RotationThreshold;
 
         public override void PlayerEnteredRange()
         {
             base.PlayerEnteredRange();
+            isIdle = false;
+            isRotating = false;
+            isShooting = true;
         }
 
-        public override void PlayerExitedRange() { }
+        public override void PlayerExitedRange() 
+        {
+            isIdle = true;
+            isRotating = false;
+            isShooting = false;
+        }
     }
 }
