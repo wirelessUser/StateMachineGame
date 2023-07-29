ac608e71050ec9aa4ea7a043a0fe335ad8d04449
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-29 16:19:42 +0530

    Create Enum for States seperately

--- Assets/Scripts/StateMachine/GenericStateMachine.cs
+++ Assets/Scripts/StateMachine/GenericStateMachine.cs
@@ -1,7 +1,5 @@
 ﻿using StatePattern.Enemy;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace StatePattern.StateMachine
 {
@@ -31,14 +29,5 @@ namespace StatePattern.StateMachine
                 state.Owner = Owner;
             }
         }
-    }
-
-    public enum States
-    {
-        IDLE,
-        ROTATING,
-        SHOOTING,
-        PATROLLING,
-        CHASING
     }
 }
